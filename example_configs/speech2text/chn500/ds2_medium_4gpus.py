189c57b2648c12242e290243798447193b8c9039
iwaterxt <iwaterxt@gmail.com>
2020-03-18 17:38:28 +0800

    update

--- example_configs/speech2text/chn500/ds2_medium_4gpus.py
+++ example_configs/speech2text/chn500/ds2_medium_4gpus.py
@@ -12,8 +12,8 @@ base_model = Speech2Text
 base_params = {
   "random_seed": 0,
   "use_horovod": False,
-  "num_gpus": 4,
-  "batch_size_per_gpu": 24,
+  "num_gpus": 2,
+  "batch_size_per_gpu": 32,
 
   "num_epochs": 50,
 
