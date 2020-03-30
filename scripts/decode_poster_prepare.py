e6255973b73566ea4f848d7a61fc8ba9546fa392
元融 <xutao.xutao@alibaba-inc.com>
2020-03-30 15:26:35 +0800

    “âupdate

--- scripts/decode_poster_prepare.py
+++ scripts/decode_poster_prepare.py
@@ -1,8 +1,8 @@
 
-import kaldiio
+import kaldi_io
 import pickle
 import numpy as np
-
+import sys
 
 
 def load_dump(pickle_file):
@@ -21,15 +21,15 @@ def softmax(x):
   return e / np.expand_dims(e.sum(axis=-1), -1)
 
 def main(pickle_file, ark_file):
-	data = load_dump(logits)
-	poster_dict = get_logits(data)
-	with open(ark_file,'wb') as f:
-  		for key,mat in poster_dict.iteritems():
-    		kaldi_io.write_mat(f, np.log(softmax(mat)), key=key)
+    data = load_dump(pickle_file)
+    poster_dict = get_logits(data)
+    with open(ark_file,'wb') as f:
+        for key,mat in poster_dict.items():
+            kaldi_io.write_mat(f, np.log(softmax(mat)), key=key)
 
 
 if __name__ == "__main__":
-	if len(sys.argc) != 3:
+	if len(sys.argv) != 3:
 		print ("python3 decode_poster_prepare.py pickle_file ark_file")
 		sys.exit(1)
 	main(sys.argv[1], sys.argv[2])
