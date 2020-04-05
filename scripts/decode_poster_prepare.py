7420bdebcc061c636e3715a150b7df503bce555c
iwaterxt <iwaterxt@gmail.com>
2020-04-05 16:49:10 +0800

    update

--- scripts/decode_poster_prepare.py
+++ scripts/decode_poster_prepare.py
@@ -39,7 +39,7 @@ def main(pickle_file, ark_file, feats_scp):
 			r,l = mat.shape
 			end_col = np.zeros((r,1))
 			end_col[:,0] = np.copy(mat[:, l-1])
-			mat_new = np.hstack((end_col, mat[:, 0:l-2]))
+			mat_new = np.hstack((end_col, mat[:, 1:l-2]))
 			kaldi_io.write_mat(f, np.log(softmax(mat_new)), key=feat2utt[key])
 
 
