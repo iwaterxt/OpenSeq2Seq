d978bd6ac69621541d34468468c3078279606152
iwaterxt <iwaterxt@gmail.com>
2020-04-11 19:30:21 +0800

    update

--- scripts/decode_poster_prepare.py
+++ scripts/decode_poster_prepare.py
@@ -37,10 +37,10 @@ def main(pickle_file, ark_file, feats_scp):
 	with open(ark_file,'wb') as f:
 		for key,mat in poster_dict.items():
 			r,l = mat.shape
-			#end_col = np.zeros((r,1))
-			mat[:,0] = np.copy(mat[:, l-1])
-			#mat_new = np.hstack((end_col, mat[:, 1:l-2]))
-			kaldi_io.write_mat(f, np.log(softmax(mat[:,0:l-1])), key=feat2utt[key])
+			end_col = np.zeros((r,1))
+			end_col = np.copy(mat[:, l-1])
+			mat_new = np.hstack((end_col, mat[:, 0:l-2]))
+			kaldi_io.write_mat(f, np.log(softmax(mat_new)), key=feat2utt[key])
 
 
 if __name__ == "__main__":
