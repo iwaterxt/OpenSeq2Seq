17d1bad7afe77bf32fdd0b8e4559379ea9e9403b
iwaterxt <iwaterxt@gmail.com>
2020-04-15 15:45:32 +0800

    update

--- open_seq2seq/encoders/ds2_encoder.py
+++ open_seq2seq/encoders/ds2_encoder.py
@@ -89,7 +89,7 @@ def subsample(name, input_layer, regularizer, skip_frames=1):
         units=D,
         kernel_regularizer=regularizer,
         activation=None,
-        name=name+'/subsample_fully_connected',
+        name='fully_connected',
     )
     return input_layer
   else:
