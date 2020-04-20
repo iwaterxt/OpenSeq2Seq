03a04fac34cc3d6cec046e34bcf38cbbc9f9c050
zhanghaiting001 <zhanghaiting001@126.com>
2020-04-20 19:52:34 +0800

    decode

---
+++ decode.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+dir=/aifs/users/tx078/tools/OpenSeq2Seq-dev/experiments/chn2000/ds2_offline_skip3_bpe_layernorm/tests
+test_dir=/aifs/users/tx078/tools/OpenSeq2Seq-dev/data/baseline_chn_2000/tests_skip3_nonorm
+gpu="0"
+
+tests="ailab_tmjl_mangbiao_dec11"
+
+for set in $tests; do
+    mkdir -p $dir/$set
+    cp /aifs/users/tx078/data/asr/tests_all/$set/text $dir/$set
+    CUDA_VISIBLE_DEVICES=${gpu} python run.py \
+        --config_file=example_configs/speech2text/chn2000/ds2_medium_4gpus_offline_skip3_bpe_layer_norm.py.bk \
+        --mode=infer \
+        --infer_output_file=$dir/$set/output \
+        || exit 1;
+
+    python scripts/decode_poster_prepare.py $dir/$set/output ark,scp:$dir/$set/poster.ark,$dir/$set/poster.scp $test_dir/$set/feats.scp || exit 1;
+    rm -rf $dir/$set/output
+
+done
+
+
